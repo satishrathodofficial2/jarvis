0ba0416da3b8c15074f242a2f64a86f4d5a64871
Satish Rathod <satishrathodofficial2@gmail.com>
2025-11-26 00:33:10 +0530

    Created complete flow

---
+++ main.py
@@ -0,0 +1,28 @@
+import speech_recognition as sr 
+import pyttsx3 
+
+def takeCommand():
+    """This function takes command & recognize
+
+    Returns:
+        text as query
+    """
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...") 
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # logging.info(e)
+        print("Say that again please")
+        return "None"
+    
+    return query
+
+takeCommand()
